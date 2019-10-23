8255b3bd51e219cbc53821527d5d7a7d6d4d7c58
huan.luo <350765740@qq.com>
2019-10-23 17:58:13 +0800

    修复api地址声明错误的问题;

--- app/src/main/java/com/lcy/base/presenter/MainPresenter.kt
+++ app/src/main/java/com/lcy/base/presenter/MainPresenter.kt
@@ -1,10 +1,10 @@
 package com.lcy.base.presenter
 
 import com.lcy.base.contract.MainContract
-import com.lcy.base.core.common.CommonSubscriber
 import com.lcy.base.core.ext.convertData
 import com.lcy.base.core.ext.rxSchedulerHelper
 import com.lcy.base.core.presenter.RxPresenter
+import com.lcy.base.core.rx.CommonSubscriber
 import com.lcy.base.data.api.ApiAppVersion
 import com.lcy.base.data.net.AppRetrofitHelper
 import com.lcy.base.data.protocol.AppVersionInfo
